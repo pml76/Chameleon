6c06b94fa843fad46c16ea1e17fac87577cb8930
pml76 <peter.lennartz@gmail.com>
2025-08-21 06:41:50 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,157 @@
+pub mod cxxqt_object;
+
+use pyo3_polars::*;
+use pyo3::prelude::*;
+use polars::prelude::*;
+use pyo3::types::{IntoPyDict, PyDict};
+use pyo3::ffi::c_str;
+use std::ffi::CStr;
+
+use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QUrl};
+
+const CODE: &CStr = c_str!(r#"
+import polars as pl
+def function(*args, **kwargs):
+    assert args == ("hello",)
+    assert kwargs == {"cruel": "world"}
+    return "called with args and kwargs"
+"#);
+
+const CODE2: &CStr = c_str!(r#"
+import numpy as np
+import polars as pl
+
+num_rows = 5000
+rng = np.random.default_rng(seed=7)
+
+buildings_data = {
+      "sqft": rng.exponential(scale=1000, size=num_rows),
+      "year": rng.integers(low=1995, high=2023, size=num_rows),
+      "building_type": rng.choice(["A", "B", "C"], size=num_rows),
+ }
+buildings = pl.DataFrame(buildings_data)
+buildings
+"#);
+
+fn pyo3_test_3() {
+    let result : Result<(), PyErr> = Python::with_gil(|py| {
+        let sys = py.import("sys")?;
+        let path = sys.getattr("path")?;
+        path.call_method1("append", ("c:\\Users\\U439644\\Projects\\Chameleon\\.venv\\Lib\\site-packages",))?;
+
+        let module = PyModule::from_code(py, CODE2, c_str!(""), c_str!(""))?;
+        let dir = module.dir()?;
+        for i in dir.iter() {
+            let s = i.extract::<String>()?;
+            let t = module.getattr(&s)?.get_type();
+            let m = t.getattr("__module__")?.extract::<String>()?;
+            if m.contains("polars") && t.name()? == "DataFrame" {
+
+               let res = PyDataFrame::extract_bound(&module.getattr(i.extract::<String>()?)?)?;
+               let res2 : DataFrame = res.into();
+               println!("{:?}", res2);
+               // println!("{} : {}.{}", i.extract::<String>()?, m, t.name()?);
+            }
+        }
+        Ok(())
+    });
+    if result.is_ok() {
+        println!("pyo3_test_3: OK");
+    } else {
+        let e = result.unwrap_err();
+        println!("pyo3_test_3: Error: {:?}", e);
+    }
+}
+
+
+fn pyo3_test_2() {
+    let result : Result<(), PyErr> = Python::with_gil(|py| {
+        let sys = py.import("sys")?;
+        let path = sys.getattr("path")?;
+        path.call_method1("append", ("c:\\Users\\U439644\\Projects\\Chameleon\\.venv\\Lib\\site-packages",))?;
+
+        let module = PyModule::from_code(py, CODE, c_str!(""), c_str!(""))?;
+        let fun = module.getattr("function")?;
+        let args = ("hello",);
+        let kwargs = PyDict::new(py);
+        kwargs.set_item("cruel", "world")?;
+        let result = fun.call(args, Some(&kwargs))?;
+        assert_eq!(result.extract::<String>()?, "called with args and kwargs");
+        Ok(())
+    });
+    if result.is_ok() {
+        println!("OK");
+    } else {
+        let e = result.unwrap_err();
+        println!("Error: {:?}", e);
+    }
+}
+
+fn polars_main() {
+    use chrono::prelude::*;
+
+    let df: DataFrame = df!(
+    "name" => ["Alice Archer", "Ben Brown", "Chloe Cooper", "Daniel Donovan"],
+    "birthdate" => [
+        NaiveDate::from_ymd_opt(1997, 1, 10).unwrap(),
+        NaiveDate::from_ymd_opt(1985, 2, 15).unwrap(),
+        NaiveDate::from_ymd_opt(1983, 3, 22).unwrap(),
+        NaiveDate::from_ymd_opt(1981, 4, 30).unwrap(),
+    ],
+    "weight" => [57.9, 72.5, 53.6, 83.1],  // (kg)
+    "height" => [1.56, 1.77, 1.65, 1.75],  // (m)
+)
+        .unwrap();
+    println!("{df}");
+
+    // Assume `df` is an existing DataFrame:
+//    let column = df.column("column_name").unwrap();
+//    let column_by_index = df.select_at_idx(0).unwrap();
+
+    let value = &df.get(0).unwrap()[1];
+    println!("{value}");
+}
+
+fn pymain() -> PyResult<()> {
+    Python::with_gil(|py| {
+        let sys = py.import("sys")?;
+        let version: String = sys.getattr("version")?.extract()?;
+
+        let locals = [("os", py.import("os")?)].into_py_dict(py)?;
+        let code = c_str!("os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'");
+        let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+
+        println!("Hello {}, I'm Python {}", user, version);
+        Ok(())
+    })
+}
 fn main() {
-    println!("Hello, world!");
+    pyo3_test_2();
+    pyo3_test_3();
+
+    polars_main();
+    let _ = pymain();
+    // Create the application and engine
+    let mut app = QGuiApplication::new();
+    let mut engine = QQmlApplicationEngine::new();
+
+    // Load the QML path into the engine
+    if let Some(engine) = engine.as_mut() {
+        engine.load(&QUrl::from("qrc:/qt/qml/com/kdab/cxx_qt/demo/qml/main.qml"));
+    }
+
+    if let Some(engine) = engine.as_mut() {
+        // Listen to a signal from the QML Engine
+        engine
+            .as_qqmlengine()
+            .on_quit(|_| {
+                println!("QML Quit!");
+            })
+            .release();
+    }
+
+    // Start the app
+    if let Some(app) = app.as_mut() {
+        app.exec();
+    }
 }
